f3b00f8d1ba29996ce21872c5983f9e5ed4d6683
arpitajain679 <arpitajain679@gmail.com>
2024-11-26 22:29:30 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Username :- admin, Password :- admin123
+
+First download the zip file and extract into Xampp htdocs folder.
+After that, import the database TODO_Sys.sql into your Xampp and run the project on localhost/TODO_Sys/ on your browser.
---
+++ include/login_header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>TODO_Sys</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/css/bootstrap.theme.min.css">
+  <link rel="stylesheet" href="assets/css/custom.css">
+  <script src="assets/js/jquery.min.js"></script>
+  <script src="assets/js/bootstrap.min.js"></script>
+  <script src="assets/js/custom.js"></script>
+  <link rel="icon" href="assets/img/60727.png">
+</head>
+<body>
+
+
+
+
+
+
+<div class="main">
